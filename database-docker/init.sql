2d6070e7ac2684666532aaa3056f09e0fcdd1347
johnformata <johnvianaluz@gmail.com>
2025-02-04 16:34:23 -0300

    criando a tabela marca_produto

---
+++ database-docker/init.sql
@@ -0,0 +1,5 @@
+-- Cria o banco se ele não existir
+CREATE DATABASE loja_virtual;
+
+-- (Opcional) Conceda privilégios a um usuário específico, se necessário
+GRANT ALL PRIVILEGES ON DATABASE loja_virtual TO postgres;
